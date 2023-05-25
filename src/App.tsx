8a9d788b1ffe0587263ba3b0f09e0fef8f68ad0b
Urichcool <104870932+Urichcool@users.noreply.github.com>
2023-05-26 00:10:13 +0200

    added-background

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Background from './components/Background'
+import Footer from './sections/Footer'
+import NavBar from './sections/NavBar'
+import Wrapper from './sections/Wrapper'
+import "./scss/index.scss"
 
 function App() {
   return (
-    <div>App</div>
+    <div className='main-container'>
+      <Background/>
+      <div className='app'>
+        <NavBar />
+        <Wrapper />
+        <Footer/>
+      </div>
+    </div>
   )
 }
 
