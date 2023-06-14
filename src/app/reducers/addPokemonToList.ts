df16dc19ac71fde5178e5133914425affacb2c46
Urichcool <104870932+Urichcool@users.noreply.github.com>
2023-06-14 15:02:58 +0200

    Update addPokemonToList.ts

--- src/app/reducers/addPokemonToList.ts
+++ src/app/reducers/addPokemonToList.ts
@@ -1,3 +1,11 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import { pokemonStatType, pokemonTypeInterface } from "../../utils/Types";
 
-export const addPokemonToList = createAsyncThunk
+export const addPokemonToList = createAsyncThunk("pokemon/addPokemon", async (pokemon: {
+    id: number;
+    name: string;
+    types: pokemonTypeInterface[] | string[]
+    stats?: pokemonStatType
+}) => {
+
+})
