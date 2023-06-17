e5d72d78938d2e0c8a5dfd607270a2f52c60f98b
Urichcool <104870932+Urichcool@users.noreply.github.com>
2023-06-18 01:57:15 +0200

    Update getUserPokemons.ts

--- src/app/reducers/getUserPokemons.ts
+++ src/app/reducers/getUserPokemons.ts
@@ -1,9 +1,50 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import { where, query, getDocs } from "firebase/firestore";
+import { pokemonListRef } from "../../utils/FirebaseConfig";
+import { defaultImages, images } from "../../utils/getPokemonImages";
+import { userPokemonsType } from "../../utils/Types";
+import { RootState } from "../store";
 
-export const getUserPokemons = createAsyncThunk("pokemon/userList", async (args, { getState }) => {
+export const getUserPokemons = createAsyncThunk(
+  "pokemon/userList",
+  async (args, { getState }) => {
     try {
-        
+      const {
+        app: { userInfo },
+      } = getState() as RootState;
+      if (!userInfo?.email) {
+        return;
+      }
+      const firestoreQuery = query(
+        pokemonListRef,
+        where("email", "==", userInfo.email)
+      );
+      const fetchedPokemons = await getDocs(firestoreQuery);
+      if (fetchedPokemons.docs.length) {
+        const userPokemons: userPokemonsType[] = [];
+        fetchedPokemons.forEach(async (pokemon) => {
+          const pokemons = await pokemon.data().pokemon;
+          //@ts-ignore
+          let image = images[pokemon.id];
+          if (!image) {
+            //@ts-ignore
+            image = defaultImages[pokemon.id];
+          }
+          const types = pokemon.types.map((name: string) => ({
+            //@ts-ignore
+            [name]: pokemon[name],
+          }));
+          userPokemons.push({
+            ...pokemons,
+            firebaseId: pokemon.id,
+            image,
+            types,
+          });
+        });
+          return userPokemons;
+      }
     } catch (error) {
-        
+      console.log(error);
     }
-})
+  }
+);
